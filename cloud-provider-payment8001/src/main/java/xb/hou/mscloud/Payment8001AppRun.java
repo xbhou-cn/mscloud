da19d6e3c7963177e0bcc8d2323237885793fd6f
xbhou-cn <47892921+xbhou-cn@users.noreply.github.com>
2021-08-30 23:37:56 +0800

    微服务提供者8001基本搭建完成

---
+++ cloud-provider-payment8001/src/main/java/xb/hou/mscloud/Payment8001AppRun.java
@@ -0,0 +1,17 @@
+package xb.hou.mscloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @title: Payment8001AppRun
+ * @Author xbhou
+ * @Date: 2021-08-30 23:07
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class Payment8001AppRun {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001AppRun.class, args);
+    }
+}
