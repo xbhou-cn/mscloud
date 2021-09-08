6e6e3f2253dcbeffa1e2389ffab6ff352b5b2562
xbhou-cn <47892921+xbhou-cn@users.noreply.github.com>
2021-09-08 23:56:17 +0800

    单机版的Eureka搭建完成

--- cloud-consumer-order80/src/main/java/xb/hou/mscloud/OrderAppRun.java
+++ cloud-consumer-order80/src/main/java/xb/hou/mscloud/OrderAppRun.java
@@ -2,6 +2,7 @@ package xb.hou.mscloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @title: OrderAppRun
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version 1.0
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderAppRun {
     public static void main(String[] args) {
         SpringApplication.run(OrderAppRun.class, args);
